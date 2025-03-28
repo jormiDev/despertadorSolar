43a35c8a25030279e87f96cc658eca9cfef00c21
pablo <79108100+jormiDev@users.noreply.github.com>
2025-03-28 09:29:47 +0100

    20250327 add lib LiquidCristal add exquema fritzing add i2c_scanner

---
+++ src/i2c_scanner.cpp
@@ -0,0 +1,23 @@
+/*
+
+#include "I2CScanner.h"
+
+I2CScanner scanner;
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial)
+  {
+  };
+
+  scanner.Init();
+}
+
+void loop()
+{
+  scanner.Scan();
+  delay(5000);
+}
+  
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
+/*
+I2C device LCD 102A - found at address 0x27
+      SCL   naranja   A5
+      SDA   marron    A4
+*/
+
+
+
+
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Crear el objeto lcd  dirección  0x27 y 16 columnas x 2 filas
+LiquidCrystal_I2C lcd(0x27, 16, 2); //
+
+void setup()
+{
+  // Inicializar el LCD
+  lcd.init();
+
+  // Encender la luz de fondo.
+  lcd.backlight();
+
+  // Escribimos el Mensaje en el LCD.
+  lcd.print("Hola Mundo");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // Ubicamos el cursor en la primera posición(columna:0) de la segunda línea(fila:1)
+  lcd.setCursor(0, 1);
+  // Escribimos el número de segundos trascurridos
+  lcd.print(millis() / 1000);
+  lcd.print(" Segundos");
+  delay(100);
 }
